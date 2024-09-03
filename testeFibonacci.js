2161365a8321221920bd0c508f44cf5c67bcbb74
Hyan <38265480+hyanmarcos@users.noreply.github.com>
2024-09-03 14:01:34 -0300

    Add files via upload

---
+++ testeFibonacci.js
@@ -0,0 +1,14 @@
+function pertenceFibonacci(num) {
+  let a = 0, b = 1;
+  while (b < num) {
+    [a, b] = [b, a + b];
+  }
+  return b === num;
+}
+
+const numero = parseInt(prompt("Digite um número:"));
+if (pertenceFibonacci(numero)) {
+  console.log(`O número ${numero} pertence à sequência de Fibonacci.`);
+} else {
+  console.log(`O número ${numero} não pertence à sequência de Fibonacci.`);
+}
